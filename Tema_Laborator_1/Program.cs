2d4c31526432c0b8e22c8bf29e920d61f4cdd327
Deny <Deny@DESKTOP-KRU3FQ3>
2026-03-03 12:41:47 +0200

    Add project files.

---
+++ Tema_Laborator_1/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace Salariu
+{
+    class Program
+    {
+        static void Main()
+        {
+            int nr_ore = 0;
+            double tarif = 0;
+
+            Console.Write("Introduceti tariful: ");
+            string tarifCaString = Console.ReadLine();
+            double.TryParse(tarifCaString, out tarif);
+
+            Console.Write("Introduceti orele lucrate: ");
+            string oreCaString = Console.ReadLine();
+            int.TryParse(oreCaString, out nr_ore);
+
+            double salariu = 0;
+            salariu = nr_ore * tarif;
+            Console.WriteLine($"Salariu: {salariu}");
+            if (salariu > 3000)
+            {
+                Console.WriteLine("Salariu mare");
+            }
+            else
+            {
+                Console.WriteLine("„Ati lucrat prea putine ore pentru a avea un salariu mare!");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
